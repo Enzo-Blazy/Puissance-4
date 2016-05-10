1c8aefbe63cb94f00db9e71c32c0bba2e1ae131a
Enzo.Blazy <enzozybla@hotmail.fr>
2016-05-10 11:58:03 +0200

    Commit with method isColumnFull and TODO Victory and Full Board.

--- src/fr/iutvalence/blazyramos/connectfour/Game.java
+++ src/fr/iutvalence/blazyramos/connectfour/Game.java
@@ -2,67 +2,66 @@ package fr.iutvalence.blazyramos.connectfour;
 
 import java.util.Scanner;
 
-/**
- * TODO.
- *
- * @author TODO
- * @version TODO
+/*
+ * Authors: Cyril Ramos, Enzo Blazy	
  */
 public class Game 
 {
-	/** TODO. */
 	private Player player1;
-	/** TODO. */
+	
 	private Player player2;
-	/** TODO. */
+	
 	private Board board;
+
 	
-	// TODO Joueur courant.
-	// TODO Tour de jeu (nombre).
-	
-	/** TODO. */
 	public Game() {
 		player1 = new Player("Momo", Piece.PIECE1);
 		player2 = new Player("Toto", Piece.PIECE2);
 		board = new Board();
-		
+
 	}
 
 	public void start() {
+		Scanner scanner = new Scanner(System.in);
+
 		System.out.printf("New game: %s vs. %s\n",player1.name(), player2.name()) ;
 		Player player = player1;
+
 		for (int i = 0; i < 999; i++) {
-	//while (!victory()) 
+			//while (!victory()) 
 			// Algo :
-				/* 1 = Afficher la board*/
-				   board.print();
-				 /* 2= Joueur courant identifi2 par sa piece*/
-				   Scanner column = new Scanner(System.in);
-				   System.out.println("Choose a column number:");
-				   int str = column.nextInt();
-				   System.out.println("You choosed the column number : " + str);
-				   
-				   
-		 		   // Verifier que colonne est bien dans la grille et v�rification de la place disponible
-				  	board.putPiece(str, player.getPiece());
-				   if (player == player1) {
-					   player = player2;   
-			   }
-			   else {
-				   	   player = player1;
-			        }
-				  
+			/* 1 = Afficher la board*/
+			board.print();
+			/* 2= Joueur courant identifié par sa piece*/
+			System.out.println("Choose a column number:");
+			int col = scanner.nextInt();
+			System.out.println("You choosed the column number : " + col);
+
+			// Vérification de la place dans la colone et positionnement du pion
+			if (!board.isFullColumn(col)) {
+				board.putPiece(col, player.getPiece());
+				if (player == player1) {
+					player = player2;   
+				}
+				else {
+					player = player1;
+				}
+			}
+			else {
+				System.err.println("The selected column is full"); //Message d'erreur
+				
+			}
+
 		}
-		         
-				   
+		scanner.close();
+
+
 	}
 
+      //TODO 
 	/*private boolean victory() {
-		for()
-		// TODO Auto-generated method stub
-		return false;
+		return board.isVictory() || board.isFull();
 	}*/
-	
 	
 }
 
