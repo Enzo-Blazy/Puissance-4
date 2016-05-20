c46f891f348877b343908452dffd7c0ef7ef1af2
Enzo.Blazy <enzozybla@hotmail.fr>
2016-05-20 14:31:43 +0200

    TODO: Victory & Wrong column number

--- src/fr/iutvalence/blazyramos/connectfour/Board.java
+++ src/fr/iutvalence/blazyramos/connectfour/Board.java
@@ -4,17 +4,18 @@ package fr.iutvalence.blazyramos.connectfour;
  * Authors: Cyril Ramos, Enzo Blazy	
  */
 public class Board {
-	
-	public static final int HEIGHT = 6;
-	
-	public static final int WIDTH = 7;
-	
-	private Piece[][] board;
-	
 
+	public static final int HEIGHT = 6;
+
+	public static final int WIDTH = 7;
+
+	private Piece[][] board;
+
+	private boolean victory;
 
 	public Board() {
 		board = new Piece[HEIGHT][WIDTH];
+		victory = false;
 	}
 
 	public void print() {
@@ -25,7 +26,7 @@ public class Board {
 				if (board[i][j] == null) {
 					System.out.print("|   ");
 				} else {
-				System.out.printf("| %s ", board[i][j]);
+					System.out.printf("| %s ", board[i][j]);
 				}
 			}
 			System.out.println("|");
@@ -37,30 +38,51 @@ public class Board {
 		for (int i=HEIGHT-1;i>=0; i--){
 			if(board[i][column] == null){
 				board [i][column] = new Piece(piece);	
-				// TODO Implement victory
-				
+				victory = checkVictory(i, column);
 				return;
 			}
 		}
 	}
-	
+
+	public boolean isVictory() {
+		return victory;
+	}
+
 	public boolean isFullColumn (int column){
 		return board[0][column] != null; 
 	}
 
-	public boolean isVictory() {
-		
+	public boolean isFull() {
+		int i = 0;
+		while (i<WIDTH && isFullColumn(i)) {
+			i++;
+		}
+		return i==WIDTH;
+	}
+
+	private boolean checkVictory(int line, int column) {
+		return checkVictoryLine(line) || checkVictoryColumn(line, column) || checkVictoryDiag1(line,column) || checkVictoryDiag2(line, column);
+	}
+
+	private boolean checkVictoryLine(int line) {
+		// TODO Auto-generated method stub
 		return false;
 	}
 
-	public void isFull(int column) {
-		for(int i=HEIGHT-1;i>=0;i--){
-			while (isFullColumn(column)&& i!=6){
-			column++;
-		}
-		}
-		return;
+	private boolean checkVictoryColumn(int line, int column) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	private boolean checkVictoryDiag1(int line, int column) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+	
+	private boolean checkVictoryDiag2(int line, int column) {
+		// TODO Auto-generated method stub
+		return false;
 	}
 }
-	
+
 
--- src/fr/iutvalence/blazyramos/connectfour/Game.java
+++ src/fr/iutvalence/blazyramos/connectfour/Game.java
@@ -8,14 +8,12 @@ import java.util.Scanner;
 public class Game 
 {
 	private Player player1;
-	
-	private Player player2;
-	
-	private Board board;
-	
-	private Scanner column;
 
-	
+	private Player player2;
+
+	private Board board;
+
+
 	public Game() {
 		player1 = new Player("Momo", Piece.PIECE1);
 		player2 = new Player("Toto", Piece.PIECE2);
@@ -29,36 +27,15 @@ public class Game
 		System.out.printf("New game: %s vs. %s\n",player1.name(), player2.name()) ;
 		Player player = player1;
 
-		for (int i = 0; i < 999; i++) {
-			//while (!victory()) 
-			// Algo :
-				/* 1 = Afficher la board*/
-				   board.print();
-				   column = new Scanner(System.in);
-				   System.out.println("Choose a column number:");
-				   int str = column.nextInt();
-				   System.out.println("You choosed the column number : " + str);
-				   
-				   
-		 		   // Verifier que colonne est bien dans la grille et v�rification de la place disponible
-				  	board.putPiece(str, player.getPiece());
-				   if (player == player1) {
-					   player = player2;   
-			   }
-			   else {
-				   	   player = player1;
-			        }
-		  
-			
-			/* 1 = Afficher la board*/
+		while (!victory()) 
 			board.print();
-			/* 2= Joueur courant identifie par sa piece*/
+
 			System.out.println("Choose a column number:");
 			int col = scanner.nextInt();
 			System.out.println("You choosed the column number : " + col);
 
-			// Verification de la place dans la colone et positionnement du pion
-			if (!board.isFullColumn(col)) {
+			// TODO Vérifier colonne				
+			if (!board.isFullColumn(col)){
 				board.putPiece(col, player.getPiece());
 				if (player == player1) {
 					player = player2;   
@@ -68,25 +45,18 @@ public class Game
 				}
 			}
 			else {
-				System.err.println("The selected column is full"); //Message d'erreur
-				
-				
-			}
-			else (board.isFull(column)){
 				System.err.println("The selected column is full");
-				
+
 			}
-		}
+			
+		
 		scanner.close();
+			}
 
-
+private boolean victory() {
+		return board.isVictory() || board.isFull();
 	}
 
-      //TODO 
-	/*private boolean victory() {
-		return board.isVictory() || board.isFull();
-	}*/
-	
 }
 
 
